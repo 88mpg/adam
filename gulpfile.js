415c91203d21c146eb2cbe0f13662be4701e12ea
Adam Fratino <fratino.a@gmail.com>
2018-01-22 12:52:36 -0500

    update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,141 +1,90 @@
-'use strict';
+// Configuration file
+const config = require('./config');
 
-// CONFIG VARIABLES
-var config = require('./config.json');
-var dest = config.dest;
+// Global requirements
+const gulp = require('gulp');
+const path = require('path');
+const browserSync = require('browser-sync');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 
-// REQUIRE VARIOUS
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var source = require('vinyl-source-stream');
-var del = require('del');
-var entityconvert = require('gulp-entity-convert');
+// HTML
+const hb = require('gulp-hb');
+const inline = require('gulp-inline');
 
-// REQUIRE HTML
-var hb = require('gulp-hb');
-var htmlmin = require('gulp-htmlmin');
-var extname = require('gulp-extname');
+// CSS
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+// const purify = require('gulp-purifycss');
 
-// REQUIRE CSS
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var nano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
-var reporter = require("postcss-reporter");
-var stylelint = require("stylelint");
+// JS
+const webpack = require('webpack2-stream-watch');
 
-// REQUIRE JS
-var browserify = require('browserify');
-var babelify = require('babelify');
-var es2015 = require('babel-preset-es2015');
-
-// REQUIRE IMAGE MINIFICATION
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-
-// BUILD HTML
-gulp.task('html', function() {
-  gulp.src(config.root + config.templates.pages + '/**/*.hbs')
-    .pipe(hb({
-      data: config.root + config.data + '/*.{js,json}',
-      helpers: config.root + config.templates.helpers + '**/*.js',
-      partials: config.root + config.templates.partials + '/**/*.hbs',
-      bustCache: true
-    }))
-    .pipe(extname('.html'))
-    .pipe(htmlmin({
-      // https://github.com/kangax/html-minifier
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true,
-      removeStyleLinkTypeAttributes: true,
-      removeScriptTypeAttributes: true,
-      useShortDoctype: true
-    }))
-    .pipe(entityconvert({ type: 'html' }))
-    .pipe(gulp.dest(dest))
-    .pipe(browserSync.reload({stream:true})); // Fire Browsersync
-});
-
-// BUILD CSS
-var supportedBrowsers = [
+const supported = [
     'last 2 versions',
     'safari >= 8',
-    'ie >= 9',
+    'ie >= 11',
     'ff >= 20',
     'ios 6',
     'android 4'
 ];
 
-gulp.task('css', function () {
-  gulp.src(config.root + config.styles + '/**/*.scss')
-    .pipe(sourcemaps.init()) // Initialize sourcemaps
-    .pipe(sass({
-      includePaths: require('node-bourbon').includePaths,
-      includePaths: require('node-neat').includePaths
-    }).on('error', sass.logError)) // Run SASS
-    .pipe(postcss([
-      // http://stylelint.io/?docs/user-guide/rules.md
-      stylelint({ /* options located in ./.stylelintrc */ }),
-      reporter({ clearMessages: true })
-    ]))
-    .pipe(nano({
-      zindex: false,
+gulp.task('html', () => {
+  gulp.src(`${config.src + config.templates.pages}**/*.hbs`)
+    .pipe(hb({
+      data: `${config.src + config.templates.data}**/*.{js,json}`,
+      helpers: `${config.src + config.templates.helpers}**/*.js`,
+      partials: `${config.src + config.templates.partials}**/*.hbs`,
+      bustCache: true
+    }))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(inline({
+      base: `${config.src}`,
+      disabledTypes: ['css', 'img', 'js']
+    }))
+    .pipe(gulp.dest(`${config.build}`))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+  gulp.src(`${config.src + config.js}**/*`)
+  .pipe(webpack( require('./webpack.config.js')))
+  .pipe(gulp.dest(`${config.build}assets/scripts`))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('styles', () => {
+  gulp.src(`${config.src + config.css.local}styles.scss`)
+  .pipe(sourcemaps.init())
+    .pipe(sass({includePaths: config.css.paths}).on('error', sass.logError))
+    // .pipe(gulpif(config.prod(), purify([`${config.build}**/*.html`, `${config.build}assets/js/**/*.js`])))
+    // .pipe(gulpif(config.prod(), cssnano({discardComments: { removeAll: true }})))
+    .pipe(cssnano({
       autoprefixer: {
-        browsers: supportedBrowsers,
+        browsers: supported,
         add: true
       }
-    })) // Run CSSNano
-    .pipe(sourcemaps.write()) // Write sourcemaps
-    .pipe(gulp.dest(dest + '/assets/css')) // Output CSS
-    .pipe(browserSync.reload({stream:true})); // Fire Browsersync
-});
-
-gulp.task('js', function() {
-  return browserify(config.root + config.scripts + '/main.js')
-    .transform("babelify", {presets: ["es2015"]})
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest(dest + '/assets/js/'))
-    .pipe(browserSync.reload({stream:true})); // Fire Browsersync
-});
-
-// MINIFY IMAGES
-gulp.task('images', function() {
-  gulp.src(config.root + config.images + '/**/*.{svg,png,gif,jpg,jpeg}')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
     }))
-    .pipe(gulp.dest(dest + config.images));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(`${config.build}assets/styles`))
+    .pipe(browserSync.stream());
 });
 
-// CLEAN BUILD
-gulp.task('clean', function() {
-  return del(dest);
+gulp.task('watch', () => {
+  gulp.watch(`${config.src + config.css.local}**/*.scss`, ['styles']);
+  gulp.watch(`${config.src + config.js}**/*.js`, ['scripts']);
+  gulp.watch([
+    `${config.src}templates/**/*.hbs`,
+    `${config.src}data/**/*.json`,
+    `${config.src + config.images}**/*.svg`
+  ], ['html']);
 });
 
-// LAUNCH BROWSERSYNC
-gulp.task('browser-sync', function() {
+gulp.task('serve',() => {
   browserSync.init({
-    // proxy: config.local + '/_build/',
-    server: './_build',
-    port: 3030,
-    open: false,
-    injectChanges: true
+    server: config.build,
+    open: false
   });
 });
-
-gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch([
-    config.root + config.templates.partials + '/**/*.hbs',
-    config.root + config.templates.pages + '/**/*.hbs',
-    config.root + config.data + '/**/*.json'], ['html']);
-  gulp.watch(config.root + config.styles + '/**/*.scss', ['css']);
-  gulp.watch(config.root + config.scripts + '/**/*.js', ['js']);
-  gulp.watch('_src/assets/images/**/*', ['images']); // can't use root
-});
-
-// TODO: clean first, build after
-gulp.task('default', ['html','css','js','images']);
